17fd137be69e7b574ff038f00ffdfb1e4098f16f
DmAs20092 <dmitry.astanovsky@gmail.com>
2026-06-03 14:17:09 +0300

    prepared goods class

--- src/components/Models/Goods.ts
+++ src/components/Models/Goods.ts
@@ -1,5 +1,30 @@
 import {IProduct, IBuyer} from '../../types';
 
 export class Goods {
-    
+    private goods: IProduct[] = [];
+    private selectedItem: IProduct | null = null;
+
+    saveProducts(products:IProduct[]): void {
+        this.goods = [...products];
+    }
+
+    getProducts(): IProduct[] {
+        return [...this.goods];
+    }
+
+    getProductById(id:string): IProduct | null {
+        return this.goods.find((product) => product.id === id) || null;
+    }
+
+    saveSelectedItem(product: IProduct): void {
+        this.selectedItem = {...product};
+    }
+
+    getSelectedItem(): IProduct | null {
+        return this.selectedItem ? {...this.selectedItem} : null;
+    }
+
+    isItemSlected(): boolean {
+        return this.selectedItem !== null;
+    }
 }
